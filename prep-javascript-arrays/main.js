6e2f6d7b3da9506f6f9d21733360e9ccd46f5d78
Dustin Davis <drpookydavis@comcast.net>
2021-11-15 21:04:09 -0800

    completed prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('language array', languages);
+
+var firstElement = 'html';
+console.log('The first element of the array is', firstElement);
+
+var thirdElement = 'javascript';
+console.log('the 3rd element of the array is', thirdElement);
+
+var length = languages.length;
+console.log('The length of the array is', length);
+
+var lastElement = 'postgres';
+console.log('The last element of the array is', lastElement);
