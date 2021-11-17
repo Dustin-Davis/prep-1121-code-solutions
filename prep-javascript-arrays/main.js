e70081fa263b0a4aa4306e993393bc9069ebd8c3
Dustin Davis <drpookydavis@comcast.net>
2021-11-16 18:13:06 -0800

    fixed error.

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,14 +1,14 @@
 var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
 console.log('language array', languages);
 
-var firstElement = 'html';
+var firstElement = languages[0];
 console.log('The first element of the array is', firstElement);
 
-var thirdElement = 'javascript';
+var thirdElement = languages[2];
 console.log('the 3rd element of the array is', thirdElement);
 
 var length = languages.length;
 console.log('The length of the array is', length);
 
-var lastElement = 'postgres';
+var lastElement = languages[5];
 console.log('The last element of the array is', lastElement);
